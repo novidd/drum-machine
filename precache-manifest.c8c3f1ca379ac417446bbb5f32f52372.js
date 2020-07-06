5f69be0d562ac8a4d118913bb09be3e2dd6ba4d9
Lucas Pettersson <clm.pettersson@gmail.com>
2020-07-06 13:52:04 +0200

    Updates

---
+++ precache-manifest.c8c3f1ca379ac417446bbb5f32f52372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5bdfafeb72bfb38f0055d223e41dc1",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "1e0ae55e4f683b15c137",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.3739f3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41202d769bc8b4995c15",
+    "url": "/drum-machine/static/js/main.d56896ac.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
